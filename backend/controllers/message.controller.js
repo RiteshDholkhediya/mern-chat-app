f6078dd6a04243736d3c7b764bc70cdc8008753c
RiteshDholkhediya <riteshkheda444@gmail.com>
2024-08-10 20:29:14 +0530

    backend completed

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,71 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id; // this will be loggedIn user id
+
+    let conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, receiverId],
+      },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    // Socket io functionality will go here
+
+    // await conversation.save();
+    // await newMessage.save();
+
+    // this will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    return res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller ", error.message);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id; //  this is comming from protectRoute middleware
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); // inside conversation we have messages array holding ids which will be converted to messages objects corresponding to their ids
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller ", error.message);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
