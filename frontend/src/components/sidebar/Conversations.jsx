3d1f4aa3a6af2bcd7bbe611182a44446cf35d53f
RiteshDholkhediya <riteshkheda444@gmail.com>
2024-08-14 13:21:24 +0530

    Ui design part completed

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,16 @@
+import Conversation from "./Conversation"
+
+
+function Conversations() {
+  return (
+    <div>
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  )
+}
+
+export default Conversations
